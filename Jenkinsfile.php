b6b141e3bbf2da7bb1b1b993f9b2e12dc4c84c40
Thoriso777 <63025042+Thoriso777@users.noreply.github.com>
2026-06-30 09:49:02 +0200

    Update Jenkinsfile.php

--- Jenkinsfile.php
+++ Jenkinsfile.php
@@ -24,15 +24,3 @@ pipeline {
         }
     }
 }
-
-public class ToUpper {
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            String input = args[0];
-            String output = input.toUpperCase();
-            System.out.println(output);
-        } else {
-            System.out.println("No input provided!");
-        }
-    }
-}
